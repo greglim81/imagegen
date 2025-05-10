6e111e2de42ed1691c805175ade852388cf7c22b
greglim81 <limjunqu@gmail.com>
2025-05-10 11:27:01 +0800

    third commit for deployment

--- src/app/api/ghibli/route.ts
+++ src/app/api/ghibli/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 import { NextRequest, NextResponse } from 'next/server';
 import Replicate from 'replicate';
 import { db, adminStorage } from '@/lib/firebaseAdmin'; // adjust path as needed
